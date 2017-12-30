976d8012fac3abeb81903621439e53c593a26967
Eulen Mesquita <eulen.mesquita@gmail.com>
2017-12-30 14:06:48 +1000

    Create message form and update incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,25 @@ socket.on('connect', function() {
 socket.on('disconnect', function() {
     console.log('Disconnected from server');
 });
-socket.on('newMessage', function(data) {
-    console.log(data);    
+socket.on('newMessage', function(message) {
+    var li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    $('#messages').append(li);
+    console.log('ok');
+});
+
+$(document).ready(function() {
+    $('#message-form').on('submit', function(e) {
+        e.preventDefault();
+        var message = $('#message');
+        socket.emit('createMessage',{
+            from: 'User',
+            text: message.val()
+        }, function() {
+
+        });
+        message.val('');
+    })
 });
 
